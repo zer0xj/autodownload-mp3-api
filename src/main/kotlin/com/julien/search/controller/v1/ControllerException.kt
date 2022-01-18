35ef2f31e209c371eaf159891c5607e9497c9fe9
zer0xj <neidballa@gmail.com>
2022-01-18 10:14:50 -0600

    Adding ControllerException.kt to project

---
+++ src/main/kotlin/com/julien/search/controller/v1/ControllerException.kt
@@ -0,0 +1,9 @@
+package com.julien.search.controller.v1
+
+import com.julien.search.model.ErrorCode
+import com.julien.search.service.BaseException
+
+
+class ControllerException(message: String?, sourceId: String?, errorCode: ErrorCode?) :
+        BaseException(message, sourceId, errorCode)
+
